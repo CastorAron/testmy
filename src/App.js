4765e346171e6021f58b32f9134e2fa265187bb8
Castor Aron <castoraron255@gmail.com>
2023-02-16 17:15:34 +0300

    node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor(){
+
+    super();
+    this.state={
+      name:{
+        firstName: 'Casto', lastname: 'Malimali'
+      }, 
+      organization:'Chanza Huru',
+      
+    };
+  }
+
+  render (){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <p>
+          Hi, My name is {this.state.name.firstName}{" "}{this.state.name.lastname}. I work at {this.state.organization}
+          <div>
+          <button onClick={()=>{
+            this.setState((state,props)=>{
+              return{
+                name:{firstName:'androvision',lastname:'Manchuria'}
+              };
+            },
+             ()=>{
+              console.log(this.state);
+            });
+          }}> 
+          Change name
+          </button>
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
+  }
+} 
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Run MY App
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// 
 export default App;
